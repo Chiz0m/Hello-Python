f8155f71c1588fa422224a39d97b93df749688f3
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-22 10:35:55 +0100

    Added Variable example file

---
+++ variable.py
@@ -0,0 +1,29 @@
+# A variable is a container that holds values e.g
+price = 10
+print(price)
+
+# You can update the price of a variable
+price = 20
+print(price)
+
+# the above varaibles are integers. We have other varialble types
+
+# flaot
+rating = 3.4
+
+# string
+name = 'Chizom'
+
+# boolean
+is_published = True
+is_published = False
+
+
+# **********************************************************************
+# here is a small program that store data of a new patient in a hospital
+# *
+# *
+# **********************************************************************
+full_name = 'John Smith'
+age = 20
+is_new = True
