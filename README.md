f0c1adf5d9a578486ffa9e3d443d39fdd0fde1f3
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-22 13:59:15 +0100

    made changes to the README and added a file to show how formatted strings work

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hello-Python
+
+Hellow python is my way of exploring python.
+This intention could change for the better and I will state it here.
---
+++ formatted_strings.py
@@ -0,0 +1,7 @@
+# formatted strings help you visualize what the output looks like in a cleaner way
+first_name = 'Chizom'
+last_name = 'Echehieuka'
+message = f'name is {first_name}'
+print(message)
+
+# I had to update my python from 2.7 to 3.8 for formatted strings to work
