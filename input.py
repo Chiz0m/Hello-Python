b7eb10a8520c9ccd6395726e6fe52fb2eb5aac70
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-22 10:47:29 +0100

    added input file to show how input is done.

---
+++ input.py
@@ -0,0 +1,10 @@
+# you can receive input from terminal e.g
+#name = input('What is your name: ')
+# print('Hi ' + name)  # this is an expression. Also know as concatenation
+
+# we can extend this program to have more functionalities when it comes to recieving inputs
+# this program request for the fullname of the user and thier fav food
+
+fullname = input('What is your full name? ')
+fav_food = input('What is your favorite food? ')
+print(fullname + ' likes ' + fav_food)
