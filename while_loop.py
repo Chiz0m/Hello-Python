af9c72db5eb6b9def4036cfc492e700d13f7c2ae
Chizom Echehieuka <chizomreal@gmail.com>
2020-08-25 11:20:34 +0100

    while loop example

---
+++ while_loop.py
@@ -0,0 +1,17 @@
+# while loops allow a section of codes to be run multiple times e.g
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print('Done')
+
+# we can also multiply a string with an integer in this while loop.
+# not to forget that there should be an increment at the end of the while loop
+# to increament the integer to be checked e.i (i)
+
+i = 1
+while i <= 5:
+    print('*' * i)
+    i += 1
+print('Done')
